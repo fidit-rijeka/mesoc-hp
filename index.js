0ab3225903536e0f005d5026e54ce736399bf137
Erik Jermaniš <erik55jermanis@gmail.com>
2021-03-13 10:39:45 +0100

    Added cookie consent message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 
 // Load enviroment variables
 dotenv.config({ path: "./config/config.env" });
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.use(cookieParser());
+
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs'
@@ -18,28 +21,41 @@ app.engine('.hbs', exphbs({
 
 app.set('view engine', '.hbs');
 
-app.get('/', (req, res) => {
+const getCookieConsent = (req, res, next) => {
+  const { cookies } = req;
+  if('mesoc_cca' in cookies) {
+    consentAccepted = 'none';
+  } else {
+    consentAccepted = 'block';
+  }
+  next();
+};
+
+app.get('/', getCookieConsent, (req, res) => {
   res.render('homepage', {
     pageTitle: 'MESOC - welcome',
     webApp: `${process.env.WEBAPP}`,
-    expressDomain: `${process.env.EXPRESS_DOMAIN}`
+    expressDomain: `${process.env.EXPRESS_DOMAIN}`,
+    cookieConsent: consentAccepted
   })
 });
 
 
 // Privacy
-app.get('/privacy', (req, res) => {
+app.get('/privacy', getCookieConsent, (req, res) => {
   res.render('privacy', {
     pageTitle: 'MESOC - Privacy',
-    webApp: `${process.env.WEBAPP}`
+    webApp: `${process.env.WEBAPP}`,
+    cookieConsent: consentAccepted
   })
 });
 
 // Terms and conditions
-app.get('/termsandconditions', (req, res) => {
+app.get('/termsandconditions', getCookieConsent, (req, res) => {
   res.render('termsandconditions', {
     pageTitle: 'MESOC - Terms and conditions',
-    webApp: `${process.env.WEBAPP}`
+    webApp: `${process.env.WEBAPP}`,
+    cookieConsent: consentAccepted
   })
 });
 
